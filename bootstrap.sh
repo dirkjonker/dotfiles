358325da67bf647ad20306a4f768a49672839887
Dirk Jonker <d.jonker@coolblue.nl>
2016-02-11 12:00:43 +0000

    Simplify and dangerify bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,28 +2,12 @@
 
 cd "$(dirname "${0}")";
 
-create_link() {
-  local src=${1} dst=${2}
-  local currentSrc=$(readlink "${dst}")
-
-  if [ "$currentSrc" == "$src" ]; then
-    return
-  fi
-
-  ln -sf "${src}" "${dst}"
-}
-
 echo "Updating..."
 git pull origin master
 
 echo "Creating symlinks..."
-for src in $(find git vim -depth 1); do
-  create_link ".dotfiles/${src}" "${HOME}/.$(basename ${src})"
+for src in $(find . -mindepth 2 -maxdepth 2 -not -path '*/\.*'); do
+  ln -fs ".dotfiles/${src}" "${HOME}/.$(basename ${src})"
 done
-
-sublimePath="${HOME}/Library/Application Support/Sublime Text 3/Packages"
-if [ -d "${sublimePath}" ]; then
-  create_link "${HOME}/.dotfiles/sublime" "${sublimePath}/User"
-fi
 
 echo "Done"
