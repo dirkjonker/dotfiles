d617ea4bc378b3e9a24d24e5184ebc6be73574dc
Dirk Jonker <dirkjonker@gmail.com>
2024-01-29 11:43:16 +0100

    Update kitty + fish to be more cross-OS compatible

--- xdg_config_home/fish/config.fish
+++ xdg_config_home/fish/config.fish
@@ -1,6 +1,27 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    eval (/opt/homebrew/bin/brew shellenv)
+    if [ -f /opt/homebrew/bin/brew ];
+        eval (/opt/homebrew/bin/brew shellenv)
+    end
+
+    if [ -f ~/.rbenv/bin/rbenv ];
+        ~/.rbenv/bin/rbenv init - fish | source
+    end
 
     fish_add_path "$HOME/.cargo/bin"
 end
+
+abbr -a gaa git add --all
+abbr -a gau git add --update
+abbr -a gcb git checkout --branch
+abbr -a gcm git switch main
+abbr -a gd git diff
+abbr -a gds git diff --staged
+abbr -a gfo git fetch origin
+abbr -a gl git pull
+abbr -a gp git push
+abbr -a grba git rebase --abort
+abbr -a grbc git rebase --continue
+abbr -a grbm git rebase main
+abbr -a gst git status
+abbr -a gsw git switch
